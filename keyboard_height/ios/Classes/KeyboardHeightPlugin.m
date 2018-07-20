80de2ec17a9bb01f0ae330ecde5b086fe008ff47
mwhite494 <mwhite494@gmail.com>
2018-07-20 16:39:05 -0400

    Added getKeyboardHeight method call for ios

--- keyboard_height/ios/Classes/KeyboardHeightPlugin.m
+++ keyboard_height/ios/Classes/KeyboardHeightPlugin.m
@@ -10,8 +10,8 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"getKeyboardHeight" isEqualToString:call.method]) {
+    result(NULL);
   } else {
     result(FlutterMethodNotImplemented);
   }
